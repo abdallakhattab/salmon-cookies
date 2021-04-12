d8bffcdca2b5dcf3faeb03cd0cbe5840e2101f06
abdallakhattab <khattababdalla@yahoo.com>
2021-04-12 14:37:20 +0300

    fixed the code

--- sls.js
+++ sls.js
@@ -4,12 +4,12 @@ function randomValue(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let a = document.getElementById ('cont')
+let contaner = document.getElementById ('cont')
  
-let hours = ['6', '7', '8', '9','10', '11', '12', '13','14' ,'15', '16' , '17 ' , '18' , '19']
+let hours = ['6am ', '7am', '8am', '9am','10am', '11am', '12pm', '1 pm','2pm' ,'3pm', '4pm' , '5pm ' , '6pm' , '7pm']
 let seattle = {
-location : seattle ,
-  min : 23 ,
+location : 'seattle' ,
+min : 23 ,
 max : 65 , 
 avg : 6.3 , 
 custumernu :[],
@@ -19,31 +19,33 @@ randomnum : function(){
 
 for (let i = 0 ; i < hours.length ; i++){
 
-this.custumernu.push
-(randomValue(this.min , this.max))
-
-
+this.custumernu.push(Math.floor(randomValue(this.min , this.max)));
 }
+console.log(this.custumernu);
+}   
 
-},
+,
 
 cookiesperhour : function(){
 
-    for(let i = 0 ; i < hours.length ; i++)
-    let val = 0 
-val = math.ceil(this.randomnum[i] * this.avg);
+    for(let i = 0 ; i < hours.length ; i++){
+    let val = 0 ;
+val = Math.floor(this.custumernu[i] * this.avg);
 this.total =this.total + val ;
-this.cookieseveryhour.push(val); 
+this.cookieseveryhour.push(val);}
+
+console.log(this.cookieseveryhour); 
+
 },
  render: function(){
 
 let h2 = document.createElement('h2');
-a.appendChild(h2);
+contaner.appendChild(h2);
 h2.textContent = this.location;
 
 let list =document.createElement('ul');
 
-a.appendChild(list);
+contaner.appendChild(list);
 
 let li = null ;
 
@@ -51,15 +53,15 @@ for ( let i = 0 ; i < this.cookieseveryhour.length ; i++){
 
 li = document.createElement('li');
 list.appendChild(li) ;
-li.textContent = this.cookieseveryhour[i];
-let totalnu = document.createElement('li');
-list.appendChild(totalnu)
-totalnu.textContent(this.total)
+li.textContent = `${hours[i]} : ${this.cookieseveryhour[i]} cookies `
+
 
 
 
 }
-
+let totalnu = document.createElement('li');
+list.appendChild(totalnu)
+totalnu.textContent = `the total is ${this.total} `
 }
 
 }
@@ -67,3 +69,5 @@ seattle.randomnum();
 console.log(seattle);
 seattle.cookiesperhour() ; 
 seattle.render();
+
+
